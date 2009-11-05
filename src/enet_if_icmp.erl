668b0628b298815222692de2785c32fd0e8e65dd
Geoff Cant <nem@erlang.geek.nz>
2009-11-05 18:23:29 +0100

    Add an ICMP ping responder.

---
+++ src/enet_if_icmp.erl
@@ -0,0 +1,141 @@
+%%%-------------------------------------------------------------------
+%% @copyright Geoff Cant
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc ENet interface icmp responder
+%% @end
+%%%-------------------------------------------------------------------
+-module(enet_if_icmp).
+
+-behaviour(gen_event).
+%% API
+-export([attach/1
+        ]).
+
+%% gen_event callbacks
+-export([init/1, handle_event/2, handle_call/2, 
+         handle_info/2, terminate/2, code_change/3]).
+
+-include("types.hrl").
+
+-record(state, {iface}).
+
+%%%===================================================================
+%%% gen_event callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Adds an event handler
+%%
+%% @spec attach() -> ok | {'EXIT', Reason} | term()
+%% @end
+%%--------------------------------------------------------------------
+attach(Interface) ->
+    gen_event:add_handler(enet_iface:event_manager(Interface),
+                          ?MODULE, [Interface]).
+
+%%%===================================================================
+%%% gen_event callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a new event handler is added to an event manager,
+%% this function is called to initialize the event handler.
+%%
+%% @spec init(Args) -> {ok, State}
+%% @end
+%%--------------------------------------------------------------------
+init([Interface]) ->
+    {ok, #state{iface=Interface}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever an event manager receives an event sent using
+%% gen_event:notify/2 or gen_event:sync_notify/2, this function is
+%% called for each installed event handler to handle the event.
+%%
+%% @spec handle_event(Event, State) ->
+%%                          {ok, State} |
+%%                          {swap_handler, Args1, State1, Mod2, Args2} |
+%%                          remove_handler
+%% @end
+%%--------------------------------------------------------------------
+handle_event({in, _Frame,
+              #eth{src=SrcEth,dst=DstEth,type=ipv4,
+                   data=#ipv4{src=SrcIP, dst=DstIP, proto=icmp,
+                              data=#icmp{type=echo_request, id=ID, seq=Seq,
+                                         data=Data}}}},
+             State = #state{iface=IF}) ->
+    Reply   = #eth{src=DstEth,dst=SrcEth,type=ipv4,
+                   data=#ipv4{src=DstIP, dst=SrcIP, proto=icmp,
+                              data=#icmp{type=echo_reply, id=ID, seq=Seq,
+                                         data=Data}}},
+    enet_iface:send(IF, Reply),
+    {ok, State};
+handle_event(_Event, State) ->
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever an event manager receives a request sent using
+%% gen_event:call/3,4, this function is called for the specified
+%% event handler to handle the request.
+%%
+%% @spec handle_call(Request, State) ->
+%%                   {ok, Reply, State} |
+%%                   {swap_handler, Reply, Args1, State1, Mod2, Args2} |
+%%                   {remove_handler, Reply}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, State) ->
+    Reply = ok,
+    {ok, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called for each installed event handler when
+%% an event manager receives any other message than an event or a
+%% synchronous request (or a system message).
+%%
+%% @spec handle_info(Info, State) ->
+%%                         {ok, State} |
+%%                         {swap_handler, Args1, State1, Mod2, Args2} |
+%%                         remove_handler
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever an event handler is deleted from an event manager, this
+%% function is called. It should be the opposite of Module:init/1 and
+%% do any necessary cleaning up.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
