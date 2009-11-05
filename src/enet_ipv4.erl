0486f1a2a7bf96dbad89d58f2f4fd524ac2e224e
Geoff Cant <nem@erlang.geek.nz>
2009-11-05 18:22:40 +0100

    Remove checksum code in favour of enet_checksum use. Rework packet expansion to fix a number of bugs.

--- src/enet_ipv4.erl
+++ src/enet_ipv4.erl
@@ -8,11 +8,13 @@
 -module(enet_ipv4).
 
 %% API
--export([decode_addr/1, encode_addr/1,
-         decode/1, decode/2, encode/1,
-         decode_protocol/1, encode_protocol/1,
-         header_checksum/1,
-         addr_len/0]).
+-export([decode_addr/1, encode_addr/1
+         ,decode/1, decode/2
+         ,encode/1, expand/1
+         ,decode_protocol/1, encode_protocol/1
+         ,header_checksum/1
+         ,addr_len/0
+        ]).
 
 -include("types.hrl").
 -define(IP_VERSION, 4).
@@ -53,20 +55,46 @@ expand(Pkt = #ipv4{options=Opts}) when is_list(Opts) ->
     expand(Pkt#ipv4{options=encode_options(Opts)});
 expand(Pkt = #ipv4{hlen=H, options=Opts}) when not is_integer(H), is_binary(Opts) ->
     expand(Pkt#ipv4{hlen=5 + (byte_size(Opts) div 4)});
-expand(Pkt = #ipv4{totlen=T, hlen=H, data=D})
-  when not is_integer(T), is_integer(H), is_binary(D) ->
-    expand(Pkt#ipv4{totlen=H + byte_size(D)});
 expand(Pkt = #ipv4{flags=Flags}) when is_list(Flags) ->
     expand(Pkt#ipv4{flags=encode_flags(Flags)});
 expand(Pkt = #ipv4{src=Src}) when not (is_binary(Src) andalso byte_size(Src) =:= 4) ->
     expand(Pkt#ipv4{src=encode_addr(Src)});
 expand(Pkt = #ipv4{dst=Dst}) when not (is_binary(Dst) andalso byte_size(Dst) =:= 4) ->
     expand(Pkt#ipv4{dst=encode_addr(Dst)});
+expand(Pkt = #ipv4{proto=P, data=D,
+                   src=Src, dst=Dst})
+  when not is_binary(D), is_atom(P), is_binary(Src),
+       is_binary(Dst) ->
+    Proto = encode_protocol(P),
+    PsuedoHdr = #ipv4_psudo_hdr{src=Src,dst=Dst,proto=Proto},
+    expand(Pkt#ipv4{data=enet_codec:encode(P, D, PsuedoHdr)});
 expand(Pkt = #ipv4{proto=P}) when not is_integer(P) ->
     expand(Pkt#ipv4{proto=encode_protocol(P)});
+expand(Pkt = #ipv4{totlen=T, hlen=H, data=D})
+  when not is_integer(T), is_integer(H), is_binary(D) ->
+    expand(Pkt#ipv4{totlen=(H*4) + byte_size(D)});
 expand(Pkt = #ipv4{hdr_csum=S}) when not is_integer(S) ->
     expand(Pkt#ipv4{hdr_csum=header_checksum(Pkt)});
-expand(Pkt) ->
+expand(Pkt = #ipv4{vsn=?IP_VERSION,
+                   hlen=HLen,
+                   diffserv=DiffServ,
+                   totlen=TotLen,
+                   id=ID,
+                   flags=Flags,
+                   frag_offset=FragOff,
+                   ttl=TTL,
+                   proto=Proto,
+                   hdr_csum=HdrChkSum,
+                   src=SrcIP,
+                   dst=DestIP,
+                   options=Options,
+                   data=Data})
+  when is_integer(HLen), is_integer(DiffServ),
+       is_integer(TotLen), is_integer(ID),
+       is_bitstring(Flags), is_integer(FragOff), is_integer(TTL),
+       is_integer(Proto),
+       is_integer(HdrChkSum), is_binary(SrcIP), is_binary(DestIP),
+       is_binary(Data), is_binary(Options) ->
     Pkt.
 
 encode(#ipv4{vsn=?IP_VERSION,
@@ -86,6 +114,7 @@ encode(#ipv4{vsn=?IP_VERSION,
   when is_integer(HLen), is_integer(DiffServ),
        is_integer(TotLen), is_integer(ID),
        is_bitstring(Flags), is_integer(FragOff), is_integer(TTL),
+       is_integer(Proto),
        is_integer(HdrChkSum), is_binary(SrcIP), is_binary(DestIP),
        is_binary(Data), is_binary(Options) ->
     OptsLen = byte_size(Options),
@@ -146,12 +175,7 @@ check_header_checksum(Dgram,HLen,HdrChkSum) when is_binary(Dgram),
                                                  is_integer(HdrChkSum) ->
     HeaderLen = 4 * HLen,
     <<Header:HeaderLen/binary, _/binary>> = Dgram,
-    OurSum = checksum(Header),
-    case OurSum of
-        0 -> correct; %% 0
-        16#FFFF -> correct; %% -0
-        _ -> {incorrect, HdrChkSum}
-    end.
+    enet_checksum:oc16_check(Header, HdrChkSum).
 
 header_checksum(#ipv4{vsn=?IP_VERSION,
                       hlen=HLen,
@@ -175,21 +199,7 @@ header_checksum(#ipv4{vsn=?IP_VERSION,
     Header = <<?IP_VERSION:4, HLen:4, DiffServ:8, TotLen:16,
               ID:16, Flags:3/bits, FragOff:13, TTL:8, Proto:8, 0:16,
               SrcIP:4/binary, DestIP:4/binary, RestDgram/binary>>,
-    checksum(Header).
-
-checksum(Bin) when is_binary(Bin) ->
-    lists:foldl(fun checksum/2,
-                0,
-                [N || <<N:16/big>> <= Bin]).
-
-%% 16 bits Ones complement addition.
-checksum(A, Sum) ->
-    case A + Sum of
-        N when N > 16#FFFF ->
-            N - 16#10000 + 1;
-        N when N =< 16#FFFF ->
-            N
-    end.
+    enet_checksum:oc16_sum(Header).
 
 decode_option_type(0,  0) -> eool;
 decode_option_type(0,  1) -> nop;
