52bfc6bc8c56dd12eea1ec8c96d69ff6b07e159c
Geoff Cant <nem@erlang.geek.nz>
2009-12-10 10:00:40 +1300

    Improve udp checksumming. Encoding a checksum doesn't work yet IIRC. FIXME.

--- src/enet_udp.erl
+++ src/enet_udp.erl
@@ -8,10 +8,15 @@
 -module(enet_udp).
 
 %% API
--export([decode/2, encode/1,
-         decode_port/1, encode_port/1]).
+-export([decode/2
+         ,expand/2
+         ,encode/2
+         ,decode_port/1
+         ,encode_port/1
+        ]).
 
 -include("types.hrl").
+-define(UDP_HEADER_LEN, 8).
 
 %%====================================================================
 %% API
@@ -20,34 +25,46 @@
 decode(<<Src:16/big, Dst:16/big,
         Length:16/big, Csum:16/big,
         Data/binary>> = Pkt,
-       [IPH = #ipv4_pseudo_hdr{} | _DecodeOpts])
-  when byte_size(Data) =:= Length - 8 ->
-    case decode_port(Dst) of
+       [IPH = #ipv4_pseudo_hdr{} | DecodeOpts])
+  when byte_size(Data) =:= Length - ?UDP_HEADER_LEN ->
+    Udp = #udp{src_port=decode_port(Src),
+               dst_port=decode_port(Dst),
+               length=Length,
+               csum=check_sum(Csum, IPH, Length, Pkt)},
+    case Udp#udp.dst_port of
         Dns when Dns =:= <<"dns">>; Dns =:= <<"mdns">> ->
-            #udp{src_port=decode_port(Src), dst_port=decode_port(Dst),
-                 length=Length, csum=check_sum(Csum, IPH, Length, Data),
-                 data=enet_dns:decode(Data)};
+            Udp#udp{data=enet_dns:decode(Data, DecodeOpts)};
         _ ->
-            #udp{src_port=decode_port(Src), dst_port=decode_port(Dst),
-                 length=Length, csum=Csum,
-                 data=Data}
+            Udp#udp{data=Data}
     end;
 decode(_Packet, _DecodeOpts) ->
     {error, bad_packet}.
 
 
 expand(Pkt = #udp{length=undefined,
-                  data=Data}) when is_binary(Data) ->
-    encode(Pkt#udp{length=byte_size(Data)});
-expand(Pkt = #udp{src_port=Src}) when not is_binary(Src) ->
-    expand(Pkt#udp{src_port=encode_port(Src)});
-expand(Pkt = #udp{dst_port=Dst}) when not is_binary(Dst) ->
-    expand(Pkt#udp{dst_port=encode_port(Dst)});
+                  data=Data}, O) when is_binary(Data) ->
+    encode(Pkt#udp{length=(byte_size(Data) + ?UDP_HEADER_LEN)}, O);
+expand(Pkt = #udp{src_port=Src}, O) when not is_binary(Src) ->
+    expand(Pkt#udp{src_port=encode_port(Src)}, O);
+expand(Pkt = #udp{dst_port=Dst}, O) when not is_binary(Dst) ->
+    expand(Pkt#udp{dst_port=encode_port(Dst)}, O);
+expand(Pkt = #udp{src_port=Src,
+                  dst_port=Dst,
+                  length=Length,
+                  csum=undefined,
+                  data=Data}, O)
+  when is_binary(Src), is_binary(Dst),
+       is_integer(Length),
+       is_binary(Data) ->
+    PseudoPkt = <<Src:16/big, Dst:16/big,
+                 Length:16/big, 0:16/big,
+                 Data:Length/binary>>,
+    expand(Pkt#udp{csum=sum(PseudoPkt, Length, O)}, O);
 expand(Pkt = #udp{src_port=Src,
                   dst_port=Dst,
                   length=Length,
                   csum=Csum,
-                  data=Data})
+                  data=Data}, _)
   when is_binary(Src), is_binary(Dst),
        is_integer(Length), is_integer(Csum),
        is_binary(Data) ->
@@ -57,15 +74,15 @@ encode(#udp{src_port=Src,
             dst_port=Dst,
             length=Length,
             csum=Csum,
-            data=Data})
+            data=Data}, _)
   when is_binary(Src), is_binary(Dst),
        is_integer(Length), is_integer(Csum),
        is_binary(Data) ->
     <<Src:16/big, Dst:16/big,
-     (Length + 8):16/big, Csum:16/big,
+     Length:16/big, Csum:16/big,
      Data:Length/binary>>;
-encode(Pkt) ->
-    encode(expand(Pkt)).
+encode(Pkt, O) ->
+    encode(expand(Pkt, O), O).
 
 decode_port(Port) ->
     enet_services:decode_port(udp, Port).
@@ -73,11 +90,20 @@ decode_port(Port) ->
 encode_port(Port) ->
     enet_services:encode_port(udp, Port).
 
+check_sum(16#FFFF, _IPH, _Length, _Data) ->
+    no_checksum;
 check_sum(Csum, #ipv4_pseudo_hdr{src=Src, dst=Dst, proto=Proto},
-          Length, Data) ->
-    Pkt = <<Src:32, Dst:32, 0:8, Proto:8, Length:16,
+          Length, Data)
+  when is_integer(Csum), is_binary(Data), is_integer(Length),
+       is_binary(Src), is_binary(Dst), is_integer(Proto) ->
+    Pkt = <<Src:4/binary, Dst:4/binary, 0:8, Proto:8/big, Length:16/big,
            Data/binary>>,
-    enet_checksum:oc16_sum(Pkt, Csum).
+    enet_checksum:oc16_check(Pkt, Csum).
+
+sum(Data, Length, #ipv4_pseudo_hdr{src=Src, dst=Dst, proto=Proto}) ->
+    Pkt = <<Src:4/binary, Dst:4/binary, 0:8, Proto:8/big, Length:16/big,
+           Data/binary>>,
+    enet_checksum:oc16_sum(Pkt).
 
 %%====================================================================
 %% Internal functions
