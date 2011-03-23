d677e901b2777d2da6b6f817087822c987005192
Geoff Cant <nem@erlang.geek.nz>
2011-03-23 15:03:31 -0700

    Move pcap definitions to a header file.

--- src/enet_pcap.erl
+++ src/enet_pcap.erl
@@ -6,19 +6,7 @@
 -module(enet_pcap).
 
 -include_lib("kernel/include/file.hrl").
--define(PCAP_MAGIC, <<16#d4,16#c3,16#b2,16#a1>>).
--define(PCAP_HDR_SIZE, 24). % 4+2+2+4+4+4+4
--define(PCAP_PKTHDR_SIZE, 16). % 4+4+4+4
--record(pcap_hdr, {version
-                   ,tz_correction
-                   ,sigfigures
-                   ,snaplen
-                   ,datalinktype
-                   ,endianess
-                  }).
--record(pcap_pkt, {ts,
-                   orig_len,
-                   data}).
+-include("enet_pcap.hrl"). 
 
 -export([foreach_file_packets/2
          ,read_file/1
