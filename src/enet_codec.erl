801a94d9f557da2ff5ac137e5a08efdcb57573c4
Geoff Cant <nem@erlang.geek.nz>
2011-09-12 18:42:20 -0700

    Many enet_codec changes * adds a behaviour * adds decompose function

--- src/enet_codec.erl
+++ src/enet_codec.erl
@@ -9,21 +9,40 @@
 
 %% API
 -export([module/1
-         ,decode/2
          ,decode/3
-         ,encode/2
          ,encode/3
+         ,encode/4
          ,packet_type/1
-         ,extract/2
          ,extract/3
+         ,extract/4
+         ,payload/2
+         ,payload/3
+         ,payload_type/2
+         ,payload_type/3
+         ,decompose/4
         ]).
+
+-export([behaviour_info/1]).
+
+%%====================================================================
+%% Behaviour
+%%====================================================================
+
+behaviour_info(callbacks) ->
+    [{decode, 2}
+     ,{payload, 2}
+     ,{payload_type, 2}
+     ,{encode, 2}
+     ,{default_options, 0}
+    ];
+behaviour_info(_) -> undefined.
 
 %%====================================================================
 %% API
 %%====================================================================
 
 module(eth) -> enet_eth;
-module(ethernet) -> enet_eth;
+module(ethernet) -> module(eth);
 module(arp) -> enet_arp;
 module(ipv4) -> enet_ipv4;
 module(ipv6) -> enet_ipv6;
@@ -31,74 +50,111 @@ module(udp) -> enet_udp;
 module(dns) -> enet_dns;
 module(icmp) -> enet_icmp;
 module(tcp) -> enet_tcp;
-module(null) -> enet_nullink.
+module(null) -> enet_nullink;
+module(pcap) -> enet_pcap;
+module(pcap_pkt) -> module(pcap);
+module(_) -> {error, unknown_packet_type}.
 
-types() ->
-    [eth, ethernet, arp, ipv4, ipv6, udp, dns, icmp, tcp, null].
-
-decode(Type, Data) ->
-    decode(Type, Data, [Type]).
-
-decode(Type, Data, [all]) ->
-    decode(Type, Data, types());
 decode(Type, Data, Options) ->
-    case lists:member(Type, Options) of
-        true ->
-            try
-                Mod = module(Type),
-                case Mod:decode(Data, Options) of
-                    {error, _} -> Data;
-                    Decoded -> Decoded
-                end
-            catch
-                C:E ->
-                    error_logger:error_msg("~p ~p:~p ~p",
-                                           [self(), C, E,
-                                            erlang:get_stacktrace()]),
-                    Data
-            end;
-        false -> Data
+    TypesToDecode = proplists:get_value(decode_types, Options, [Type]),
+    true = TypesToDecode =:= all orelse lists:member(Type, TypesToDecode),
+    {Mod, MOpts} = mod_options(Type, Options),
+    true = Mod =/= error,
+    case Mod:decode(Data, MOpts) of
+        {error, _} -> Data;
+        Decoded -> Decoded
     end.
 
-encode(Type, Data) ->
-    Mod = module(Type),
-    Mod:encode(Data).
+encode(Type, Data, Options) ->
+    {Mod, MOpts} = mod_options(Type, Options),
+    Mod:encode(Data, MOpts).
 
-encode(Type, Data, OuterPacket) ->
-    Mod = module(Type),
-    Mod:encode(Data, OuterPacket).
+encode(Type, Data, OuterPacket, Options) ->
+    {Mod, MOpts} = mod_options(Type, Options),
+    Mod:encode(Data, OuterPacket, MOpts).
 
 packet_type(T) when is_tuple(T) ->
-    Tag = element(1, T),
-    case lists:member(Tag, types()) of
-        true -> Tag;
-        false -> erlang:error({badarg, Tag})
-    end.
+    element(1, T).
 
-extract(TargetType, Packet) when is_tuple(Packet) ->
-    extract(TargetType, packet_type(Packet), Packet).
+extract(TargetType, Packet, Options) when is_tuple(Packet) ->
+    extract(TargetType, packet_type(Packet), Packet, Options).
 
-extract(TargetType, Type, Packet) ->
-    Mod = module(Type),
-    case Mod:payload_type(Packet) of
+extract(TargetType, Type, Packet, Options) when is_tuple(Packet) ->
+    {Mod, MOpts} = mod_options(Type, Options),
+    case Mod:payload_type(Packet, MOpts) of
         TargetType ->
-            TargetPacket = Mod:payload(Packet),
+            TargetPacket = Mod:payload(Packet, MOpts),
             case TargetPacket of
                 L when is_list(L) ->
-                    find_type(TargetType, L);
+                    find_type(TargetType, L, Options);
                 _ -> TargetPacket
             end;
         IntermediateType when IntermediateType =/= Type ->
-            extract(TargetType, IntermediateType, Mod:payload(Packet))
+            extract(TargetType, IntermediateType,
+                    Mod:payload(Packet, MOpts), Options)
     end.
 
-find_type(TargetType, []) -> erlang:error({missing, TargetType});
-find_type(TargetType, [Packet | Rest]) when is_tuple(Packet) ->
+find_type(TargetType, [], _) -> erlang:error({missing, TargetType});
+find_type(TargetType, [Packet | Rest], Options) when is_tuple(Packet) ->
     case packet_type(Packet) of
         TargetType -> Packet;
-        _ -> find_type(TargetType, Rest)
+        _ -> find_type(TargetType, Rest, Options)
+    end.
+
+payload_type(Pkt, Options) ->
+    PktType = packet_type(Pkt),
+    payload_type(PktType, Pkt, Options).
+
+payload_type(PktType, Pkt, Options) ->
+    {Mod, MOpts} = mod_options(PktType, Options),
+    Mod:payload_type(Pkt, MOpts).
+
+payload(Pkt, Options) ->
+    PktType = packet_type(Pkt),
+    payload(PktType, Pkt, Options).
+
+payload(PktType, Pkt, Options) ->
+    {Mod, MOpts} = mod_options(PktType, Options),
+    Mod:payload(Pkt, MOpts).
+
+decompose(Types, PktType, Pkt, Options) ->
+    decompose(Types, PktType, Pkt, Options, []).
+
+decompose(Types, PktType, Pkt, Options, Layers) when is_binary(Pkt)->
+    case mod_options(PktType, Options) of
+        {error, unknown_packet_type} -> Layers;
+        {Mod, MOpts} ->
+            Packet = Mod:decode(Pkt, MOpts),
+            decompose(Types, PktType, Packet, Options, Layers)
+    end;
+decompose(Types, PktType, Packet, Options, Layers) when is_tuple(Packet) ->
+    Acc = [Packet | Layers],
+    case mod_options(PktType, Options) of
+        {error, unknown_packet_type} ->
+            Acc;
+        {Mod, Opts} ->
+            try Mod:payload_type(Packet, Opts) of
+                PayloadType ->
+                    case lists:member(PayloadType, Types) of
+                        true ->
+                            Payload = Mod:payload(Packet, Opts),
+                            decompose(Types, PayloadType, Payload,
+                                      Options, Acc);
+                        false -> Acc
+                    end
+            catch error:undef -> Acc
+            end
     end.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+mod_options(Type, Options) ->
+    case module(Type) of
+        {error, _} = E -> E;
+        Mod when is_atom(Mod) ->
+            Defaults = try Mod:default_options()
+                       catch _:_ -> Options end,
+            {Mod, proplists:get_value(Mod, Options, Defaults)}
+    end.
