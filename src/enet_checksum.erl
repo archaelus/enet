bcfc1ba37bf3d7e7b99ff911d3db11fbc43bcc66
Geoff Cant <nem@erlang.geek.nz>
2017-05-24 20:19:16 -0700

    Fixes #2 16bit ones complement checksum code.
    Replaced with code from Feuerlabs https://github.com/Feuerlabs/enet/blob/f9ec108230786ef579323cb8bfafff73e3a3d577/src/enet_checksum.erl used with permission: https://github.com/archaelus/enet/issues/2

--- src/enet_checksum.erl
+++ src/enet_checksum.erl
@@ -19,28 +19,31 @@
 %%====================================================================
 
 %% 16bit Ones complement checksum of a given binary.
-oc16(Bin) when is_binary(Bin) ->
-    lists:foldl(fun oc16/2,
-                0,
-                [N || <<N:16>> <= Bin]).
+oc16(Bin)
+  when is_binary(Bin) ->
+    oc16(Bin, 0).
+
+oc16(<<A:16,B:16,Bin/binary>>,Sum) -> oc16(Bin, A+B+Sum);
+oc16(<<A:16,B:8>>, Sum)  -> oc16_fold(A+(B bsl 8)+Sum);
+oc16(<<A:16>>, Sum)  -> oc16_fold(A+Sum);
+oc16(<<A:8>>, Sum) -> oc16_fold((A bsl 8)+Sum);
+oc16(<<>>, Sum) -> oc16_fold(Sum).
 
 %% Calculate the ones complement of the 16bit ones complement sum of
 %% the input binary.
-oc16_sum(Bin) ->
+oc16_sum(Bin)
+  when is_binary(Bin) ->
     (bnot oc16(Bin)) band 16#FFFF.
 
-%% 16 bits Ones complement addition.
-oc16(A, Sum) ->
-    case A + Sum of
-        N when N > 16#FFFF ->
-            Carry = N bsr 16,
-            (N band 16#FFFF) + Carry;
-        N when N =< 16#FFFF ->
-            N
-    end.
-
-oc16_check(Bin, Sum) ->
+oc16_check(Bin, Sum)
+  when is_binary(Bin) ->
     case oc16(Bin) of
         16#FFFF -> correct; %% -0
         _ -> {incorrect, Sum}
     end.
+
+%% fold 16-bit carry
+oc16_fold(Sum) when Sum > 16#ffff ->
+    oc16_fold((Sum band 16#ffff) + (Sum bsr 16));
+oc16_fold(Sum) ->
+    Sum.
