d40a7f160b69dba8f2d9d54442b234570f3cb481
Geoff Cant <nem@erlang.geek.nz>
2017-05-31 13:42:19 -0700

    Add printable ascii to enet_if_dump:hexblock/1.
    Extends the formatting done by hexblock to include the printable ascii sidebar that Wireshark includes.

--- src/enet_if_dump.erl
+++ src/enet_if_dump.erl
@@ -147,7 +147,8 @@ hexblock_line(Offset, Line) ->
     %"0x0000:  4500 0045 0000 0000 4001 f564 c0a8 0202  E..E....@..d...."
     Groups = [ io_lib:format("~4.16.0b", [Group]) || << Group:16 >> <= Line ],
     JGroups = string:join(Groups, " "),
-    io_lib:format("0x~4.16.0b:  ~s~n", [Offset, JGroups]).
+    io_lib:format("0x~4.16.0b:  ~s ~s~n", [Offset, JGroups,
+                                           to_printable(Line)]).
 
 hexblock_lastline(Offset, Line) ->
     Size = byte_size(Line),
@@ -159,9 +160,25 @@ hexblock_lastline(Offset, Line) ->
     JGroups = string:join(Groups, " "),
     case LastGroupSize of
         0 ->
-            io_lib:format("0x~4.16.0b:  ~s~n", [Offset, JGroups]);
+            io_lib:format("0x~4.16.0b:  ~s ~s~n", [Offset, JGroups,
+                                                  to_printable(Line)]);
         _ ->
-            io_lib:format("0x~4.16.0b:  ~s~n",
-                          [Offset, [JGroups, " ",
-                                    erlang:integer_to_list(LastGroup, 16)]])
+            Hex = [JGroups, " ",
+                   erlang:integer_to_list(LastGroup, 16)],
+            %% Full line would be 8 groups of 4 hex digits with 7
+            %% spaces interspersed (4*8)+7
+            Padding = binary:copy(<<" ">>, (4*8 + 7) - iolist_size(Hex)),
+            io_lib:format("0x~4.16.0b:  ~s ~s~n",
+                          [Offset, Hex,
+                           [Padding,
+                           to_printable(Line)]])
     end.
+
+to_printable(Bin) when is_binary(Bin) ->
+    [ case C of
+          Printable when 16#20 =< C, C =< 16#7E ->
+              Printable;
+          _NonPrintable ->
+              $.
+      end
+       || <<C>> <= Bin ].
